b69a4b6a480363e021c4c23f5eac3ffc8d6adfde
kattrujillo1016 <ktruji25@msudenver.edu>
2021-04-14 11:30:35 -0600

    initializing first repo

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office, require */
+
+const ssoAuthHelper = require("./../helpers/ssoauthhelper");
+
+Office.onReady(info => {
+  if (info.host === Office.HostType.Outlook) {
+    document.getElementById("addSignatures").onclick = addSign;
+
+   
+  }
+});
+// finish tomorrow morning
+function addSign(){
+  var sign = document.querySelector('[name="email"]').value;
+  var ul = document.getElementById("myList");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(sign));
+  ul.appendChild(li);
+
+  console.log(sign);
+}
+
+
+function function1() {
+  var ul = document.getElementById("myList");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode("newEmail"));
+  ul.appendChild(li);
+}
